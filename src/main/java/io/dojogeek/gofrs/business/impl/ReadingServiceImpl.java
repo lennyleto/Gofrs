e4d1caee3c2b68f2f6e29f0b688353bbf0206ffb
Irene Gutierrez <igutierrez@dojogeek.io>
2016-03-01 20:00:34 -0600

    Create ReadingServiceImpl Class

---
+++ src/main/java/io/dojogeek/gofrs/business/impl/ReadingServiceImpl.java
@@ -0,0 +1,28 @@
+package io.dojogeek.gofrs.business.impl;
+
+import io.dojogeek.gofrs.business.ReadingService;
+import io.dojogeek.gofrs.rest.entities.Reading;
+import org.springframework.stereotype.Service;
+
+/**
+ * Created by Irene on 3/1/16.
+ */
+@Service
+public class ReadingServiceImpl implements ReadingService {
+
+    public Reading createReading(Reading reading) {
+        return null;
+    }
+
+    public Reading updateReading(String id, Reading reading) {
+        return null;
+    }
+
+    public Reading deleteReading(String id) {
+        return null;
+    }
+
+    public Reading getReadingsByUserId(String userId) {
+        return null;
+    }
+}

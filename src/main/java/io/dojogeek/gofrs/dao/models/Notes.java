69c13f7cba52bcc761a6b6ab5ed6a66118567f0f
Irene Gutierrez <igutierrez@dojogeek.io>
2016-03-03 23:39:00 -0600

    Add Persistence annotations for "Notes" entity

--- src/main/java/io/dojogeek/gofrs/dao/models/Notes.java
+++ src/main/java/io/dojogeek/gofrs/dao/models/Notes.java
@@ -3,14 +3,32 @@ package io.dojogeek.gofrs.dao.models;
 import lombok.Data;
 import org.joda.time.DateTime;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
 /**
  * Created by Irene on 3/1/16.
  */
 @Data
+@Entity
+@Table(name = "notes")
 public class Notes {
+
+    @Id
+    @Column(name = "id")
     private String id;
+
+    @Column(name = "title")
     private String title;
+
+    @Column(name = "date")
     private DateTime date;
+
+    @Column(name = "body")
     private String body;
+
+    @Column(name = "idBook")
     private String idBook;
 }

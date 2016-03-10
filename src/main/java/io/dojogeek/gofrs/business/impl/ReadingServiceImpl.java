1a031c2ad3f8510c4de29302c525df9582797658
Irene Gutierrez <igutierrez@dojogeek.io>
2016-03-10 15:58:22 -0600

    Add "createReading" operation

--- src/main/java/io/dojogeek/gofrs/business/impl/ReadingServiceImpl.java
+++ src/main/java/io/dojogeek/gofrs/business/impl/ReadingServiceImpl.java
@@ -1,7 +1,12 @@
 package io.dojogeek.gofrs.business.impl;
 
 import io.dojogeek.gofrs.business.ReadingService;
+import io.dojogeek.gofrs.business.mapper.ReadingMapper;
+import io.dojogeek.gofrs.dao.ReadingDao;
+import io.dojogeek.gofrs.dao.models.ReadingModel;
 import io.dojogeek.gofrs.rest.entities.Reading;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Service;
 
 /**
@@ -10,8 +15,16 @@ import org.springframework.stereotype.Service;
 @Service
 public class ReadingServiceImpl implements ReadingService {
 
+    @Autowired
+    ReadingDao readingDao;
+
+    @Autowired
+    @Qualifier("reading-mapper")
+    private ReadingMapper readingMapper;
+
     public Reading createReading(Reading reading) {
-        return null;
+        ReadingModel readingModel = readingDao.createReading(readingMapper.mapToInner(reading));
+        return readingMapper.mapToOuter(readingModel);
     }
 
     public Reading updateReading(String id, Reading reading) {

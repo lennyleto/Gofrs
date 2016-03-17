2fe3f5e5499c40945d0aad61f19cd5635f9aef2b
Irene Gutierrez <igutierrez@dojogeek.io>
2016-03-17 15:31:44 -0600

    Modify data types

--- src/main/java/io/dojogeek/gofrs/rest/entities/Book.java
+++ src/main/java/io/dojogeek/gofrs/rest/entities/Book.java
@@ -1,7 +1,9 @@
 package io.dojogeek.gofrs.rest.entities;
 
+import io.dojogeek.gofrs.rest.adapters.DateFormatterAdapter;
 import lombok.Data;
-import org.joda.time.DateTime;
+import javax.xml.bind.annotation.adapters.XmlJavaTypeAdapter;
+import java.util.Date;
 
 /**
  * Created by Irene on 2/18/16.
@@ -12,7 +14,8 @@ public class Book {
     private String id;
     private String title;
     private String author;
-    private DateTime releaseDate ;
+    @XmlJavaTypeAdapter(DateFormatterAdapter.class)
+    private Date releaseDate ;
     private String editorial;
     private String category;
     private int totalPages;

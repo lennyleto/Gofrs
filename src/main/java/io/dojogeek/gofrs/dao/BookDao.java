6ec6881d479cabf643d91d06be28a56ec9e53ad0
Irene Gutierrez <igutierrez@dojogeek.io>
2016-03-17 15:25:04 -0600

    Add implementation Dao

--- src/main/java/io/dojogeek/gofrs/dao/BookDao.java
+++ src/main/java/io/dojogeek/gofrs/dao/BookDao.java
@@ -1,7 +1,17 @@
 package io.dojogeek.gofrs.dao;
 
+import io.dojogeek.gofrs.dao.models.BookModel;
+
 /**
  * Created by Irene on 3/17/16.
  */
 public interface BookDao {
+
+    BookModel createBook(BookModel bookModel);
+
+    BookModel getBookByUserId(String userId);
+
+    BookModel updateBook(BookModel bookModel);
+
+    void deleteBook(String id);
 }

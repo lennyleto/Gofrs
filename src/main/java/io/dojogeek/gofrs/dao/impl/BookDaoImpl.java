d417bed1f7b1a82f2953fe346c9c5f3927ba3ac4
Irene Gutierrez <igutierrez@dojogeek.io>
2016-03-17 18:29:45 -0600

    Add "createBook" operation

--- src/main/java/io/dojogeek/gofrs/dao/impl/BookDaoImpl.java
+++ src/main/java/io/dojogeek/gofrs/dao/impl/BookDaoImpl.java
@@ -2,15 +2,22 @@ package io.dojogeek.gofrs.dao.impl;
 
 import io.dojogeek.gofrs.dao.BookDao;
 import io.dojogeek.gofrs.dao.models.BookModel;
+import io.dojogeek.gofrs.util.HibernateUtil;
+import org.hibernate.Session;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 /**
  * Created by Irene on 3/17/16.
  */
 @Repository
-public class BookDaoImpl implements BookDao {
+@Transactional
+public class BookDaoImpl extends HibernateUtil implements BookDao {
+
     public BookModel createBook(BookModel bookModel) {
-        return null;
+        Session session = getCurrentSession();
+        session.save(bookModel);
+        return bookModel;
     }
 
     public BookModel getBookByUserId(String userId) {

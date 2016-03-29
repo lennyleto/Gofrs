4971357192101bc10d3c883558ca7f427c810514
Irene Gutierrez <igutierrez@dojogeek.io>
2016-03-29 17:56:36 -0600

    Add @Service annotation

--- src/main/java/io/dojogeek/gofrs/business/impl/NoteServiceImpl.java
+++ src/main/java/io/dojogeek/gofrs/business/impl/NoteServiceImpl.java
@@ -2,12 +2,14 @@ package io.dojogeek.gofrs.business.impl;
 
 import io.dojogeek.gofrs.business.NoteService;
 import io.dojogeek.gofrs.rest.entities.Notes;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 
 /**
  * Created by Irene on 3/29/16.
  */
+@Service
 public class NoteServiceImpl implements NoteService {
 
     public Notes createNote(Notes notes) {

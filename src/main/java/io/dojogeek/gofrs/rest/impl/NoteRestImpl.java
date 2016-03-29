82ed1f276c02bbfb7b69046fae479036b6675023
Irene Gutierrez <igutierrez@dojogeek.io>
2016-03-29 17:57:40 -0600

    Add @Produces and @Consumes annotations

--- src/main/java/io/dojogeek/gofrs/rest/impl/NoteRestImpl.java
+++ src/main/java/io/dojogeek/gofrs/rest/impl/NoteRestImpl.java
@@ -2,12 +2,21 @@ package io.dojogeek.gofrs.rest.impl;
 
 import io.dojogeek.gofrs.rest.NoteRest;
 import io.dojogeek.gofrs.rest.entities.Notes;
+import org.springframework.stereotype.Service;
 
+import javax.ws.rs.Consumes;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 
 /**
  * Created by Irene on 3/29/16.
  */
+@Path("/note")
+@Service
+@Produces(MediaType.APPLICATION_JSON)
+@Consumes(MediaType.APPLICATION_JSON)
 public class NoteRestImpl implements NoteRest {
 
     public Response createNote(Notes notes) {

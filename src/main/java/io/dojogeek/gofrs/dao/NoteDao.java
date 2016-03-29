f1d745a33d867ab12a9560f3182afdee29347d20
Irene Gutierrez <igutierrez@dojogeek.io>
2016-03-29 16:40:44 -0600

    Add Implementation

--- src/main/java/io/dojogeek/gofrs/dao/NoteDao.java
+++ src/main/java/io/dojogeek/gofrs/dao/NoteDao.java
@@ -1,7 +1,19 @@
 package io.dojogeek.gofrs.dao;
 
+import io.dojogeek.gofrs.dao.models.NotesModel;
+
+import java.util.List;
+
 /**
  * Created by Irene on 3/29/16.
  */
 public interface NoteDao {
+
+    NotesModel createNote(NotesModel notesModel);
+
+    List<NotesModel> getNotesByUserId(String userId);
+
+    NotesModel updateNote(NotesModel notesModel);
+
+    void deleteNote(String id);
 }

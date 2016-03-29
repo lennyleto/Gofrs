397b27bfdc5a639a252cc26f042243680ac0cc76
Irene Gutierrez <igutierrez@dojogeek.io>
2016-03-29 17:57:09 -0600

    Add @Repository and @Transactional annotation

--- src/main/java/io/dojogeek/gofrs/dao/impl/NoteDaoImpl.java
+++ src/main/java/io/dojogeek/gofrs/dao/impl/NoteDaoImpl.java
@@ -2,12 +2,16 @@ package io.dojogeek.gofrs.dao.impl;
 
 import io.dojogeek.gofrs.dao.NoteDao;
 import io.dojogeek.gofrs.dao.models.NotesModel;
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
 /**
  * Created by Irene on 3/29/16.
  */
+@Repository
+@Transactional
 public class NoteDaoImpl implements NoteDao {
 
     public NotesModel createNote(NotesModel notesModel) {

1ca155c84f069433f2aca81e92522b08b13798de
Irene Gutierrez <igutierrez@dojogeek.io>
2016-03-29 16:40:49 -0600

    Add Implementation

--- src/main/java/io/dojogeek/gofrs/rest/NoteRest.java
+++ src/main/java/io/dojogeek/gofrs/rest/NoteRest.java
@@ -1,8 +1,19 @@
 package io.dojogeek.gofrs.rest;
 
+import io.dojogeek.gofrs.rest.entities.Notes;
+
+import javax.ws.rs.core.Response;
+
 /**
  * Created by Irene on 3/29/16.
  */
 public interface NoteRest {
 
+    Response createNote(Notes notes);
+
+    Response getNotesByUserId(String userId);
+
+    Response updateNote(String id, Notes notes);
+
+    Response deleteNote(String id);
 }

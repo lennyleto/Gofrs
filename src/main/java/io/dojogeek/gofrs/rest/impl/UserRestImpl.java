72483340ba07f2e68cc0ec0526d9974bb7100abb
Irene Gutierrez <igutierrez@dojogeek.io>
2016-04-14 18:26:31 -0500

    Add annotations

--- src/main/java/io/dojogeek/gofrs/rest/impl/UserRestImpl.java
+++ src/main/java/io/dojogeek/gofrs/rest/impl/UserRestImpl.java
@@ -1,7 +1,18 @@
 package io.dojogeek.gofrs.rest.impl;
 
+import org.springframework.stereotype.Service;
+
+import javax.ws.rs.Consumes;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
 /**
  * Created by Irene on 4/13/16.
  */
+@Path("/user")
+@Service
+@Produces(MediaType.APPLICATION_JSON)
+@Consumes(MediaType.APPLICATION_JSON)
 public class UserRestImpl {
 }
